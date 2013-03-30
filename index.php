bd84fb1d8bd9305171562373e4d33fb9673459a8
Christopher Krug <christopherkrug@monitoroklahoma.com>
2013-03-30 16:33:49 -0500

    Added single-primary
    Not a correction but did fix a problem. Will clean up later.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="site-content">
+	<div id="single-primary" class="site-content">
 		<div id="content" role="main">
 			<?php if ( have_posts() ) : ?>
 				<?php /* Start the Loop */ ?>
