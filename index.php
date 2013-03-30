3c4fdc5518f4111424338e647da8b5a6b0793761
Christopher Krug <christopherkrug@monitoroklahoma.com>
2013-03-30 12:12:10 -0500

    Cleaning up the loop for our needs
    Removed get_template_part() to grab content.php. Left .entry-title in notes mode to use later.

--- index.php
+++ index.php
@@ -20,7 +20,16 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content', get_post_format() ); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			                        <header class="entry-header">
+			                            <h1 class="entry-title">
+			                                    <!--<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>-->
+			                            </h1>
+			                        </header><!-- .entry-header -->
+			                        <div class="entry-content">
+			                                <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
+			                        </div><!-- .entry-content -->
+			                    </article><!-- #post -->
 				<?php endwhile; ?>
 				<?php hundredstories_content_nav(); ?>
             <?php else : ?>
