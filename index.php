da7ef445288355c7fce37234316140e2c23688de
Christopher Krug <christopherkrug@monitoroklahoma.com>
2013-03-30 12:16:04 -0500

    Seduced into aligning tabs
    ditto

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  */
 
 get_header(); ?>
-  <div id="primary" class="site-content">
+	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php if ( have_posts() ) : ?>
 				<?php /* Start the Loop */ ?>
@@ -35,7 +35,7 @@ get_header(); ?>
             		<?php else : ?>
 				<?php get_template_part( 'content', 'none' ); ?>
 			<?php endif; ?>
-			</div><!-- #content -->
-		</div><!-- #primary -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
 <?php get_sidebar();?>
 <?php get_footer(); ?>
