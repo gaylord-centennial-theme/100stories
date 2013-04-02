b36f42e41ec0e51c7911e8035c9dd3eda66d278c
taystaab <taystaab@ou.edu>
2013-04-02 15:51:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,21 +21,21 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			                        <header class="entry-header">
-			                            <h1 class="entry-title">
-			                                    <!--<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>-->
-			                            </h1>
-			                        </header><!-- .entry-header -->
-			                        <div class="entry-content">
-			                                <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
-			                        </div><!-- .entry-content -->
-			                    </article><!-- #post -->
+                        <header class="entry-header">
+                            <h1 class="entry-title">
+                                    <!--<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>-->
+                            </h1>
+                        </header><!-- .entry-header -->
+                        <div class="entry-content">
+                                <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( '%s', 'hundredstories' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
+                        </div><!-- .entry-content -->
+                    </article><!-- #post -->
 				<?php endwhile; ?>
 				<?php hundredstories_content_nav(); ?>
-            		<?php else : ?>
-				<?php get_template_part( 'content', 'none' ); ?>
+            <?php else : ?>
+				
 			<?php endif; ?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+			</div><!-- #content -->
+		</div><!-- #primary -->
 
 <?php get_footer(); ?>
